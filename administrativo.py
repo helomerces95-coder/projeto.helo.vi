8bd7159fc50f1fc2180283ed3eaa9e3e1366baa9
lorranevivian23-lgtm <lorranevivian23@gmail.com>
2025-10-30 09:18:51 -0300

    atualização no arquivo de adm, FINALIZADO

--- administrativo.py
+++ administrativo.py
@@ -3,4 +3,16 @@ from cliente import Cliente
 class Administrador(Pessoa):   #classe adm vai herdar de pessoa
     def __init__(self, nome, cpf, idade):
         super().__init__(nome, cpf, idade):
-        
+        self.clientes = []
+
+def cancelar_agendamento(self, cliente: Cliente):
+        cliente.agendamento = None
+        cliente.procedimento = None
+        cliente.forma_pagamento = None
+        print(f"Agendamento de {cliente.nome} cancelado.")
+
+def historico_tratamentos(self, cliente: Cliente):
+        print(f"Histórico de tratamentos de {cliente.nome}: {cliente.procedimento if cliente.procedimento else 'Nenhum tratamento realizado.'}")
+
+def faturamento(self, cliente: Cliente):
+        print(f"Forma de pagamento de {cliente.nome}: {cliente.forma_pagamento if cliente.forma_pagamento else 'Não definido'}")
