3d4f679501ac2a09af6de9cf4db2a04ce0d4ab40
lorranevivian23-lgtm <lorranevivian23@gmail.com>
2025-10-31 08:04:18 -0300

    finalizado

--- procedimento.py
+++ procedimento.py
@@ -18,10 +18,35 @@ class Botox(Procedimentofacial):
     def __init__(self):
         super().__init__("Botox", 600)
 
-class Tratamentoacne(Procedimentofacial):
-    def __init__(self, nome, preco):
-        super().__init__(Acne, preco)
-    
+class TratamentoAcne(Procedimentofacial):
+    def __init__(self):
+        super().__init__("Tratamento para Acne", 300)
+
 class ProcedimentoCorporal(Procedimento):
     def __init__(self, nome, preco):
         super().__init__(nome, preco)
+
+class Drenagem(ProcedimentoCorporal):
+    def __init__(self):
+        super().__init__("Drenagem", 150)
+
+class MassagemModeladora(ProcedimentoCorporal):
+    def __init__(self):
+        super().__init__("Massagem Modeladora", 180)
+
+class TratamentoEstrias(ProcedimentoCorporal):
+    def __init__(self):
+        super().__init__("Tratamento de Estrias", 250)
+
+# Procedimentos Capilares
+class ProcedimentoCapilar(Procedimento):
+    def __init__(self, nome, preco):
+        super().__init__(nome, preco)
+
+class TratamentoQueda(ProcedimentoCapilar):
+    def __init__(self):
+        super().__init__("Tratamento para Queda Capilar", 300)
+
+class HidrataçãoCapilar(ProcedimentoCapilar):
+    def __init__(self):
+        super().__init__("Hidratação Capilar", 200)
